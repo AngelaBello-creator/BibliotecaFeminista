94d4c2b3ce9f65bb0b8ee0f0ee7822de5be39cb4
Andrea <aoliverar.96@gmail.com>
2025-10-06 09:26:38 +0200

    Model-Entities Done

--- src/main/java/com/library/model/entities/Genre.java
+++ src/main/java/com/library/model/entities/Genre.java
@@ -1,3 +1,57 @@
+package com.library.model.entities;
+
+
 public class Genre {
-    
+    private Integer id;
+    private String name;
+
+  
+    public Genre() {
+    }
+
+    public Genre(Integer id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public Genre(String name) {
+        this.name = name;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "Genre{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Genre genre = (Genre) o;
+        return name != null ? name.equals(genre.name) : genre.name == null;
+    }
+
+    @Override
+    public int hashCode() {
+        return name != null ? name.hashCode() : 0;
+    }
 }
